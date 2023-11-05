0e64a3bf354d9f857f1b0e1b6b480deafa0085f3
Ashwini Kumar <ashwinikumar@microsoft.com>
2023-11-05 22:14:36 +0530

    remove dummy data and show shimmer UI

--- component/Body.js
+++ component/Body.js
@@ -1,20 +1,24 @@
 
 import ResturantCard from "./ResturantCard";
+import  Shimmer  from "./Shimmer";
 import { useState, useEffect } from 'react';
-import { resList } from "../utils/mockData";
 
 const Body=()=>{
-    const [listOfResturant, setListOfResturant]=useState(resList);
+    const [listOfResturant, setListOfResturant]=useState([]);
     useEffect(()=>{fethcData()},[]);
     
     const fethcData=async()=>{
         const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-        const json= await data.json();
+        const json= await data.json(); 
         console.log(data);
         console.log(json);
         console.log(json?.data?.cards[5]?.card?.card.gridElements?.infoWithStyle?.restaurants);
         console.log(json.data.cards[2].card.card.gridElements?.infoWithStyle?.restaurants);
-        setListOfResturant(json.data.cards[2].card.card.gridElements?.infoWithStyle?.restaurants);
+        setListOfResturant(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+
+    if(listOfResturant?.length<=0){
+        return <Shimmer/>
     }
 
     return (
@@ -29,7 +33,7 @@ const Body=()=>{
             </div>
             <div className='res-container'>
                 {
-                    listOfResturant.map(resturant =>(
+                    listOfResturant?.map(resturant =>(
                         <ResturantCard key={resturant.id} resData={resturant} />
                     ))
                 }
