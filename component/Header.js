99d27f89e19ee59e59502b15c90f8f8b79476578
Ashwini Kumar <ashwinikumar@microsoft.com>
2023-11-27 21:13:47 +0530

    Use Link tag instead of a tag- doing client side routing instead of server side routing

--- component/Header.js
+++ component/Header.js
@@ -1,5 +1,6 @@
 import logo from '../static/img/logo.png';
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const Header=()=>{
     const [btnName,setBtnName]=useState("Login");
@@ -18,9 +19,11 @@ const Header=()=>{
             </div>
             <div className='nav-items'>
                 <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/about'>About</Link></li>
+                    <li>
+                        <Link to='/contact'>Contact Us</Link>
+                    </li>
                     <li>Cart</li>
                     <button onClick={()=>{btnName=="Login"?(setBtnName("Logout")):setBtnName("Login")}}>{btnName}</button>
 
