5293651a6e34e2162f6c84b75b6a20b84db8ed4b
Ashwini Kumar <ashwinikumar@microsoft.com>
2024-01-01 20:52:05 +0530

    understand more about redux toolkit and how it used to work previously

--- utils/cartSlice.js
+++ utils/cartSlice.js
@@ -7,6 +7,14 @@ const cartSlice=createSlice({
     },
     reducers:{
         addItem:(state,action)=>{
+
+            // Vanilla(older) Redux=> Don't mutate the state, and returning the state was mandatory
+            // const newState=[...state];
+            // newState.items.push(item);
+            // return newState;
+
+            // in Redux Toolkit, we can mutate the state and returning is also not mandatory
+            // Redux Toolkit uses immer behind the scenes
             // mutating the state over here
             state.items.push(action.payload);
         },
@@ -15,6 +23,8 @@ const cartSlice=createSlice({
             // we can write better logic here
         },
         clearCart:(state,action)=>{
+            // RTK says- either Mutate the existing state or return a new state
+            // state.items.length=0; or //return {items:[]} 
             state.items.length=0;
         }
     }
